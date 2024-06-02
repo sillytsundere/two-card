dd461869f481c11b490960c4ad2678892b14afd1
Paige <paigehcarroll@gmail.com>
2024-06-02 13:13:56 -0500

    initial commit, set up client with vite and basic server with express, installed npm packages

---
+++ server/server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+//import Express.js
+const express = require('express');
+const path = require('path');
+
+// Initialize instance of Express.js
+const app = express();
+// Specify on which port the Express.js server will run
+const PORT = process.env.PORT || 3001;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.get('/test', (req, res) => {
+    res.json('Testing, testing...')
+});
+
+// json body parser for express
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// });
+
+app.get('/test', (req, res) => {
+    res.json('Testing, testing...')
+});
+
+// listen() method is responsible for listening for incoming connections on the specified port 
+app.listen(PORT, () =>
+console.log(`App is now listening at http://localhost:${PORT}`)
+);
